8e4b0c164937d9126c45857bdc3d4678c6edda40
Anurag <anuragabcr@gmail.com>
2025-01-26 13:45:50 +0530

    display image in view

--- app/details/[hotelID]/page.tsx
+++ app/details/[hotelID]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Loading from "@/components/Loading";
 import { HotelInfoDetails } from "@/lib/HotelDetails";
@@ -6,80 +6,110 @@ import { getDocumentById } from "@/lib/firestore";
 import { useEffect, useState } from "react";
 
 interface EditHotelProps {
-    params: {
-        hotelID: string;
-    };
+  params: {
+    hotelID: string;
+  };
 }
 const EditHotel: React.FC<EditHotelProps> = ({ params: { hotelID } }) => {
-    const [loading, setLoading] = useState(true)
-    const initialFormData: HotelInfoDetails = new HotelInfoDetails();
-    const [formData, setFormData] = useState<HotelInfoDetails >(initialFormData);
+  const [loading, setLoading] = useState(true);
+  const initialFormData: HotelInfoDetails = new HotelInfoDetails();
+  const [formData, setFormData] = useState<HotelInfoDetails>(initialFormData);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const temp = await getDocumentById(hotelID);
-          if (temp !== null) setFormData(temp);
-          setLoading(false)
-        };
-    
-        fetchData();
-      }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const temp = await getDocumentById(hotelID);
+      if (temp !== null) setFormData(temp);
+      setLoading(false);
+    };
 
-    if (loading) {
-        return <Loading />
+    fetchData();
+  }, [hotelID]);
+
+  const isValidUrl = (url: string): boolean => {
+    try {
+      new URL(url);
+      return true;
+    } catch (error) {
+      return false;
     }
-    
-    return (
-        <div className="flex items-center justify-center m-2">
-        <div className="xl:w-10/12 w-full px-8">
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="flex items-center justify-center m-2">
+      <div className="xl:w-10/12 w-full px-8">
         <div className="bg-white overflow-hidden shadow rounded-lg border">
-    <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">
-            User Profile
-        </h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-            This is some information about the user.
-        </p>
-    </div>
-    <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-        <dl className="sm:divide-y sm:divide-gray-200">
-        {Object.entries(formData).map(([key, value]) => (
-        <div key={key} className="mb-2">
-            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">
-                    {key}
-                </dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {typeof value === 'object' ? (<div className="sm:divide-y sm:divide-gray-200">
-                    {Object.entries(value).map(([key2, value2]) => (
-                        <div key={key2} className="mb-2">
-                        <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt className="text-sm font-medium text-gray-500">
-                                {key2}
-                            </dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            {typeof value === 'object' ? JSON.stringify(value2) : value}   
-                            </dd>
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Hotel Profile
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              This is some information about the hotel.
+            </p>
+          </div>
+          <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+            <dl className="sm:divide-y sm:divide-gray-200">
+              {Object.entries(formData).map(([key, value]) => (
+                <div key={key} className="mb-2">
+                  <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">{key}</dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      {typeof value === "string" && isValidUrl(value) ? (
+                        <img
+                          width={500}
+                          height={300}
+                          src={value}
+                          alt={key}
+                          className="max-w-full h-auto rounded-lg"
+                        />
+                      ) : typeof value === "object" ? (
+                        <div className="sm:divide-y sm:divide-gray-200">
+                          {Object.entries(value).map(([key2, value2]) => (
+                            <div key={key2} className="mb-2">
+                              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                  {key2}
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                  {typeof value2 === "string" &&
+                                  isValidUrl(value2) ? (
+                                    <img
+                                      width={500}
+                                      height={300}
+                                      src={value2}
+                                      alt={key2}
+                                      className="max-w-full h-auto rounded-lg"
+                                    />
+                                  ) : (
+                                    JSON.stringify(value2)
+                                  )}
+                                </dd>
+                              </div>
                             </div>
-                            </div>
-                    ))}
-                </div>) : value}
-                </dd>
-            </div>
+                          ))}
+                        </div>
+                      ) : (
+                        value
+                      )}
+                    </dd>
+                  </div>
+                </div>
+              ))}
+            </dl>
+          </div>
         </div>
-      ))}
-            
-        </dl>
+      </div>
     </div>
-</div>
-</div>
-</div>
-    )
-}
+  );
+};
 
-export default EditHotel
+export default EditHotel;
 
-{/* <div className="bg-white rounded-lg shadow-md p-6">
+{
+  /* <div className="bg-white rounded-lg shadow-md p-6">
       <h2 className="text-xl font-bold mb-4">Hotel Details</h2>
       {Object.entries(formData).map(([key, value]) => (
         <div key={key} className="mb-2">
@@ -87,4 +117,5 @@ export default EditHotel
           <p className="ml-2">{typeof value === 'object' ? JSON.stringify(value) : value}</p>
         </div>
       ))}
-    </div> */}
+    </div> */
+}
